aa6e5de7a422a92558f9f9cbc3980e57b73f276a
Simon <33012273+5E7EN@users.noreply.github.com>
2024-10-08 00:20:09 +0300

    Hides images and recommended playlist

---
+++ README.md
@@ -0,0 +1,10 @@
+# Spicetify - Hide Pritzus
+
+Spiceify extension that hides recommended playlists and images possibly containing pritzus from the Spotify desktop client.
+
+## Installation
+
+0. Install [Spicetify](https://spicetify.app/docs/getting-started)
+1. Place file into `C:\Users\sevdev\AppData\Roaming\spicetify\Extensions` folder
+2. Run `spicetify config extensions hidePritzus.js`
+3. Run `spicetify apply`
---
+++ hidePritzus.js
@@ -0,0 +1,75 @@
+const replaceImagesWithGrey = () => {
+    // Hide all <img> tags
+    const allImages = document.querySelectorAll('img');
+
+    allImages.forEach((img) => {
+        // Replace the image source with the grey image
+        img.src = '';
+        // Clear any srcset attributes
+        img.srcset = '';
+        // Ensure the greyed image fits in nicely
+        img.style.objectFit = 'cover';
+    });
+
+    // Hide CSS background images
+    const allElements = document.querySelectorAll('*');
+    allElements.forEach((element) => {
+        const bgImage = window.getComputedStyle(element).backgroundImage;
+
+        // Check if the element has a background image
+        if (bgImage !== 'none') {
+            // Check if the image is a URL
+            if (bgImage.includes('url')) {
+                // Remove only the url() part of the string
+                const url = bgImage.replace(/url\(['"]?(.*?)['"]?\)/, 'url()');
+
+                // Replace the background image with the grey image
+                element.style.backgroundImage = url;
+            }
+        }
+    });
+};
+
+(async function extension() {
+    // Waits for platform to load before running
+    const UserAPI = Spicetify?.Platform?.UserAPI;
+    if (!UserAPI) {
+        setTimeout(extension, 500);
+        return;
+    }
+
+    // const user = await Spicetify.Platform.UserAPI.getUser();
+    Spicetify.showNotification('Shmiras Einayim ACTIVATED!');
+
+    const removePlaylists = () => {
+        const elementsForRemoval = [];
+
+        //* Israel Region
+        // Stay Tuned
+        elementsForRemoval.push(document.querySelectorAll('[aria-label="תשארו מעודכנים"]'));
+
+        //* America Region
+        // Episodes for you
+        // elementsForRemoval.push(document.querySelectorAll('[aria-label="Episodes for you"]'));
+
+        // Popular * (e.g. Popular Albums, Popular Playlists)
+        // elementsForRemoval.push(document.querySelectorAll('[aria-label^="Popular"]'));
+
+        // Featured * (e.g. Featured Charts)
+        // elementsForRemoval.push(document.querySelectorAll('[aria-label^="Featured"]'));
+
+        // Spotify * (e.g. Spotify Playlists)
+        // elementsForRemoval.push(document.querySelectorAll('[aria-label^="Spotify"]'));
+
+        // Remove elements
+        for (const element of elementsForRemoval) {
+            element.forEach((item) => item.remove());
+        }
+    };
+
+    const rpId = setInterval(removePlaylists, 2000);
+    const riwgId = setInterval(replaceImagesWithGrey, 100);
+
+    console.log(`SE-RP: ${rpId}`);
+    console.log(`SE-RIWG: ${riwgId}`);
+})();
